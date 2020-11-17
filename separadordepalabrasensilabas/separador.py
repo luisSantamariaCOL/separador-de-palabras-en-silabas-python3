7708666e48b1916f5a2c8cea6d0edbe305d93fe0
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2020-11-17 05:20:50 -0500

    definiendo métodos

--- separadordepalabrasensilabas/separador.py
+++ separadordepalabrasensilabas/separador.py
@@ -1,1 +1,32 @@
 # Separador de Palabras en Sílabas, Python 3
+
+def main():
+    print("hola mundo")
+
+# Separa una palabra en silabas
+def separarPalabraEnSilabas(palabra):
+    return None
+
+
+# determina si un string es una palabra
+def esPalabra(texto):
+    return True
+
+# determina si un pedazo de string es una consonante
+def esConsonante(texto):
+    return True
+
+# Determina si un pedazo de string es una vocal
+def esVocal(texto):
+    return True
+
+# Determina si dos letras unidas forman un grupo consonantico
+def esGrupoConsonantico(consonante1, consonante2):
+    return True
+
+# Determina si una letra es una vocal
+def esVocalFuerte(vocal):
+    return True
+
+if __name__ == "__main__":
+    main()
