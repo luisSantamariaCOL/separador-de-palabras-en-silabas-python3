3ddd0af775bdb3f37680d464434a1dacfd9f02a9
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2020-11-21 23:39:41 -0500

    metodo esConsonante funcionando

--- separadordepalabrasensilabas/separador.py
+++ separadordepalabrasensilabas/separador.py
@@ -3,8 +3,7 @@
 def main():
     palabras = ["transgresor", "123", "monstruo", "murcielago", "abecedario", "palabras", "sol",
                 "dia", "luna", "idea", "consejo", "enciclopedia", "genio", "mariscal#"]
-    for palabra in palabras:
-        print(esPalabra(palabra))
+
 
 
 # Separa una palabra en silabas
@@ -23,8 +22,12 @@ def esPalabra(texto):
             es_palabra = False
     return es_palabra
 
-# determina si un pedazo de string es una consonante
-def esConsonante(texto):
+# determina si una letra es una consonante; es decir, si no es una vocal.
+def esConsonante(letra):
+    no_consonantes = ["a","e","i","o","u",
+                   "á","é","í","ó","ú"]
+    if letra in no_consonantes:
+        return False
     return True
 
 # Determina si un pedazo de string es una vocal
